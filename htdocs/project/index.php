a7f990d1f7976f07c925a406caf805e089eae48f
Sarim Kamran <sarimk4779@gmail.com>
2023-11-19 12:08:33 -0700

    dummy code

--- htdocs/project/index.php
+++ htdocs/project/index.php
@@ -1,3 +1,29 @@
+<html>
+<body>
+
+
+<h1> Test </h1>
+
 <?php
     echo "Hello World"
 ?>
+
+<?php
+//Create connection
+$con=mysqli_connect("localhost", "cpsc471_project", "1234", "art_gallery");
+
+//Check connection
+if (mysqli_connect_errno()){
+    echo "Failed to connect to SQL".mysqli_connect_error();
+}
+?>
+
+<?php
+mysqli_close($con);
+?>
+
+
+</body>
+</html>
+
+
