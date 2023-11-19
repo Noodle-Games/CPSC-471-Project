08a0efcff59624b81c4f217642a07f7e665976e2
Sarim Kamran <sarimk4779@gmail.com>
2023-11-19 12:47:12 -0700

    test code to enter data into database

--- htdocs/project/index.php
+++ htdocs/project/index.php
@@ -5,10 +5,11 @@
 <h1> Test </h1>
 
 <?php
-    echo "Hello World"
+    echo "Hello World";
 ?>
 
 <?php
+
 //Create connection
 $con=mysqli_connect("localhost", "cpsc471_project", "1234", "art_gallery");
 
@@ -16,9 +17,18 @@ $con=mysqli_connect("localhost", "cpsc471_project", "1234", "art_gallery");
 if (mysqli_connect_errno()){
     echo "Failed to connect to SQL".mysqli_connect_error();
 }
-?>
 
-<?php
+$sql = "INSERT INTO artist (email, Fname, Lname) VALUES ('yo@girl.com', 'Sara', 'Lynn')";
+
+if (!mysqli_query($con, $sql)){
+    die ('Error:' . mysqli_error($con));
+}
+else{
+    echo "1 record added";
+}
+
+
+
 mysqli_close($con);
 ?>
 
