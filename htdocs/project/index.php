acfa9a62f08866549b19bab6885fbe2b2de2e27e
Sarim Kamran <sarimk4779@gmail.com>
2023-11-23 15:50:08 -0700

    PHP/SQL edits

--- htdocs/project/index.php
+++ htdocs/project/index.php
@@ -3,17 +3,14 @@
 
 
 <h1> Welcome to the Gallery </h1>
-
-<?php
-    echo "Meet our artists";
-?>
+<h2>Meet our artists</h2>
 
 <?php
 
-//Create connection
+// Create SQL connection
 $con = mysqli_connect("localhost", "cpsc471_project", "1234", "art_gallery");
 
-//Check connection
+// Check SQL connection
 if (mysqli_connect_errno()){
     echo "Failed to connect to SQL".mysqli_connect_error();
 }
@@ -31,13 +28,15 @@ if (mysqli_connect_errno()){
 // FOR SELECTING/DISPLAYING
 $result = mysqli_query($con, "SELECT * FROM artist");
 
-echo "<table border='1'>
+// Table headings
+echo "<table border='2'>
 <tr>
 <th> Email </th>
 <th> Fname </th>
 <th> Lname </th>
 </tr>";
 
+// Looping through select query to display in table
 while($row = mysqli_fetch_array($result)){
     echo "<tr>";
     echo "<td>" . $row['email']. "</td>";
@@ -47,9 +46,80 @@ while($row = mysqli_fetch_array($result)){
 }
 echo "</table>";
 
+// SEARCH FUNCTION
+// Search variables
+$artwork_id_search = $artworkErr = "";
 
+// Reference 1: https://www.w3schools.com/php/php_form_complete.asp
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (empty($_POST["artwork_id_search"])) {
+      $artworkErr = "Please Enter an artwork ID";
+    } else {
+      $artwork_id_search = test_input($_POST["artwork_id_search"]);
+      // check if name only contains letters and whitespace
+      if (!preg_match("/^[a-zA-Z0-9' ]*$/",$artwork_id_search)) {
+        $artworkErr = "Only letters, numbers, and white space allowed";
+      }
+    }
+}
+
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
+// SQL query to display artwork
+function display_artwork($art_id){
+
+    $artwork_result = mysqli_query($con, "SELECT * FROM artwork");
+
+    echo "<table border='2'>
+    <tr>
+    <th> Artwork ID </th>
+    <th> Year </th>
+    <th> Artist Email </th>
+    <th> Store ID </th>
+    </tr>";
+
+    while($row = mysqli_fetch_array($artwork_result)) {
+        echo "<tr>";
+        echo "<td>" . $row['artwork_ID']. "</td>";
+        echo "<td>" . $row['year']. "</td>";
+        echo "<td>" . $row['artist_email']. "</td>";
+        echo "<td>" . $row['store_id']. "</td>";
+        echo "</tr>";
+    }
+    
+    echo "Hello";
+}
+
+?>
+
+<!--Reference 1-->
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+    <br><br>
+    Search Artwork: <input type="text" name="artwork_id_search" value="<?php echo $artwork_id_search;?>">
+    <span> <?php echo $artworkErr;?></span>
+    <br><br>
+    <input type="submit" name="submit" value="Search">
+</form>
+
+<!--Reference 1-->
+<?php
+echo "<h2>Input Check:</h2>";
+echo $artwork_id_search;
+echo "<br><br>";
+
+//display_artwork($artwork_id_search);
+?>
+
+<?php 
+// Close Connection
 mysqli_close($con);
 ?>
+
 
 
 </body>
