9e1565d7805ac1af981acd00a113c81f114a7986
darpalp <darpalpatel123@gmail.com>
2023-12-09 16:00:34 -0700

    live auction updates bid

--- htdocs/project/live_auctions.php
+++ htdocs/project/live_auctions.php
@@ -1,82 +1,87 @@
-<html>
-
-<head>
-<link rel="stylesheet" href="galleryStyle.css">
-</head>
-
-
-<button class="button button_home" onclick="location='index.php'">Logout</button>
-<button class="button button_home" onclick="location='index_cust.php'">Home Page</button>
-
-<div class="div1">
-    <h1> Calgary Art Market </h1>
-    <h2> Live Auctions </h2>
-</div>
-
-<body>
-
 <?php
 // Create SQL connection
 $con = mysqli_connect("localhost", "cpsc471_project", "1234", "art_gallery");
 
 // Check SQL connection
-if (mysqli_connect_errno()){
-    echo "Failed to connect to SQL".mysqli_connect_error();
+if (!$con) {
+    die("Failed to connect to SQL: " . mysqli_connect_error());
 }
+?>
 
-// AUCTION DISPLAY
-$query = "SELECT * FROM auction";
-$auction = mysqli_query($con, $query);
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <link rel="stylesheet" href="galleryStyle.css">
+    <title>Calgary Art Market</title>
+</head>
 
-while ($row = mysqli_fetch_array($auction)) {
-    echo '<div class="auction-item">';
-    echo '<img src="">';
-    echo '<div class="details">';
-    echo '<p><strong>Starting Bid:</strong> $' . $row['starting_bid'] . '</p>';
-    echo '<p><strong>Highest Bid:</strong> $' . $row['highest_bid'] . '</p>';
-    echo '<p><strong>End Date:</strong> ' . $row['end_date'] . '</p>';
-    echo '<div class="bid">';
-    echo '<form method="post" action="' . $_SERVER['PHP_SELF'] . '">';
-    echo '<input type="hidden" name="artwork_id" value="' . $row['artwork_id'] . '">';
-    echo 'Place Bid: $<input type="text" name="bid_amount">';
-    echo '<input type="submit" value="Submit Bid">';
-    echo '</form>';
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $artwork_id = $_POST["artwork_id"];
-        $bid_amount = $_POST["bid_amount"];
+<body>
 
-        if (!is_numeric($bid_amount) or $bid_amount <=  $row['highest_bid']) {
-            echo "Invalid bid amount";
-            return;
-        } 
-        else{
-            $bid_amount = floatval($bid_amount);
-            $updateQuery = "UPDATE auction AS A SET A.highest_bid = $bid_amount WHERE A.artwork_id = $artwork_id";
-            echo "Update Query: " . $updateQuery;
-            $auction = mysqli_query($con, $updateQuery);
+    <button class="button button_home" onclick="location='index.php'">Logout</button>
+    <button class="button button_home" onclick="location='index_cust.php'">Home Page</button>
 
-            if ($con->query($update_query) === TRUE) {
-                echo "Bid placed successfully!";
+    <div class="div1">
+        <h1>Calgary Art Market</h1>
+        <h2>Live Auctions</h2>
+    </div>
+
+    <?php
+    // AUCTION DISPLAY
+    $query = "SELECT * FROM auction";
+    $auction = mysqli_query($con, $query);
+
+    while ($row = mysqli_fetch_array($auction)) {
+        echo '<div class="auction-item">';
+        echo '<img src="">';
+        echo '<div class="details">';
+        echo '<p><strong>Starting Bid:</strong> $' . $row['starting_bid'] . '</p>';
+        echo '<p><strong>Highest Bid:</strong> $' . $row['highest_bid'] . '</p>';
+        echo '<p><strong>End Date:</strong> ' . $row['end_date'] . '</p>';
+        echo '<div class="bid">';
+        echo '<form method="post" action="' . $_SERVER['PHP_SELF'] . '">';
+        echo '<input type="hidden" name="artwork_id" value="' . $row['artwork_id'] . '">';
+        echo 'Place Bid: $<input type="text" name="bid_amount">';
+        echo '<input type="submit" name="submit" value="Submit Bid">';
+        echo '</form>';
+
+        if (isset($_POST["submit"])) {
+            $artwork_id = $_POST["artwork_id"];
+            $bid_amount = $_POST["bid_amount"];
+
+            // Validate bid amount
+            if (!is_numeric($bid_amount) or $bid_amount <= $row['highest_bid']) {
+                echo "Invalid bid amount";
+            } else {
+                $bid_amount = floatval($bid_amount);
+
+                $updateQuery = "UPDATE auction SET highest_bid = ? WHERE artwork_id = ?";
+                $updateStatement = $con->prepare($updateQuery);
+                $updateStatement->bind_param("di", $bid_amount, $artwork_id);
+
+                if ($updateStatement->execute()) {
+                    echo "Bid placed successfully!";
+                    header("Location: ".$_SERVER['PHP_SELF']);
                 } else {
-                    echo "Error updating bid: " . $con->error;
+                    echo "Error updating bid: " . $updateStatement->error;
                 }
+
+                $updateStatement->close();
+            }
         }
 
-        
+        echo '</div>';
+        echo '</div>';
+        echo '</div>';
     }
-    echo '</div>';
-    echo '</div>';
-    echo '</div>';
-}
-?>
+    ?>
 
-
-<?php 
-mysqli_close($con);
-?>
+    <?php
+    mysqli_close($con);
+    ?>
 
 </body>
+
 </html>
 
 
